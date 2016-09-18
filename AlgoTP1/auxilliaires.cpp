3fd12a561ac3318f1b42e7056fea93c861c61924
stefan_l <leon.stefani@epitech.eu>
2016-09-18 17:57:08 -0400

    Add hours utils

--- AlgoTP1/auxilliaires.cpp
+++ AlgoTP1/auxilliaires.cpp
@@ -46,11 +46,15 @@ void lireFichier(std::string nomFichier, std::vector<std::vector<std::string>>& 
     }
 }
 
-Date::Date()
-{
+static inline tm& getLocalTime() {
     auto now = std::chrono::system_clock::now();
     time_t tt = std::chrono::system_clock::to_time_t(now);
-    tm local_tm = *localtime(&tt);
+    return *localtime(&tt);
+}
+
+Date::Date()
+{
+    tm local_tm = getLocalTime();
     m_an = reinterpret_cast<unsigned int>(local_tm.tm_year);
     m_mois = reinterpret_cast<unsigned int>(local_tm.tm_mon);
     m_jour = reinterpret_cast<unsigned int>(local_tm.tm_mday);
@@ -63,7 +67,7 @@ Date::Date(unsigned int an, unsigned int mois, unsigned int jour)
 }
 
 bool Date::operator==(const Date &other) const {
-    return *this < other && *this > other;
+    return !(*this < other) && !(*this > other);
 }
 
 bool Date::operator<(const Date &other) const {
@@ -76,6 +80,59 @@ bool Date::operator>(const Date &other) const {
 
 friend std::ostream & Date::operator<<(std::ostream &flux, const Date &p_date) {
     flux << 'Dates printing not implemented yet';
+    return flux;
+}
+
+Heure::Heure() {
+    tm local_tm = getLocalTime();
+    m_heure = reinterpret_cast<unsigned int>(local_tm.tm_hour);
+    m_min = reinterpret_cast<unsigned int>(local_tm.tm_min);
+    m_sec = reinterpret_cast<unsigned int>(local_tm.tm_sec);
+}
+
+Heure::Heure(unsigned int heure, unsigned int min, unsigned int sec)
+    : m_heure(heure), m_min(min), m_sec(sec)
+{
+    ;
+}
+
+bool Heure::operator<(const Heure &other) const {
+    return std::tie(m_heure, m_min, m_sec) < std::tie(other.m_heure, other.m_min, other.m_sec);
+}
+
+bool Heure::operator>(const Heure &other) const {
+    return std::tie(m_heure, m_min, m_sec) > std::tie(other.m_heure, other.m_min, other.m_sec);
+}
+
+bool Heure::operator==(const Heure &other) const {
+    return !(*this < other) && !(*this > other);
+}
+
+bool Heure::operator>=(const Heure &other) const {
+    return *this > other || *this == other;
+}
+
+bool Heure::operator<=(const Heure &other) const {
+    return *this < other || *this == other;
+}
+
+Heure Heure::add_secondes(unsigned int secs) const {
+    Heure newHour(*this);
+    newHour.m_sec += secs;
+    newHour.m_min += m_sec / 60;
+    newHour.m_sec %= 60;
+    newHour.m_heure += m_min / 60;
+    newHour.m_min %= 60;
+    if (newHour.m_heure > 30) newHour.m_heure = 30;
+    return  newHour;
+}
+
+int Heure::operator-(const Heure &other) const {
+    return (m_heure * 3600 + m_min * 60 + m_sec) - (other.m_heure * 3600 + other.m_min * 60 + other.m_sec);
+}
+
+friend std::ostream & operator<<(std::ostream & flux, const Heure & p_heure) {
+    flux << 'Hours printing not implemented yet';
     return flux;
 }
 
