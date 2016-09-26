1bd6e5eb72f10dc5c954c348e3678fd0abb2f486
potierg <guillaume.potier@epitech.eu>
2016-09-25 23:44:05 -0400

    fix methods

--- AlgoTP1/coordonnees.cpp
+++ AlgoTP1/coordonnees.cpp
@@ -2,28 +2,32 @@
 #include <math.h>
 #include "Enonce/coordonnees.h"
 
-Coordonnees::Coordonnees(double latitude, double longitude) : m_latitude(latitude), m_longitude(longitude)
+Coordonnees::Coordonnees(double latitude, double longitude)
 {
+	if (!Coordonnees::is_valide_coord(latitude, longitude))
+		return ;
+	m_latitude = latitude;
+	m_longitude = longitude;
 }
 
 double Coordonnees::getLatitude() const
 {
-	return (this->m_latitude);
+	return (m_latitude);
 }
 
 void Coordonnees::setLatitude(double latitude)
 {
-	this->m_latitude = latitude;
+	m_latitude = latitude;
 }
 
 double Coordonnees::getLongitude() const
 {
-	return (this->m_longitude);
+	return (m_longitude);
 }
 
 void Coordonnees::setLongitude(double longitude)
 {
-	this->m_longitude = longitude;
+	m_longitude = longitude;
 }
 
 bool Coordonnees::is_valide_coord(double p_latitude, double p_longitude)
@@ -40,7 +44,11 @@ double Coordonnees::operator-(const Coordonnees & other) const
 	double rayon = 6371;
 	double pi = 3.141592;
 
-	return (2.0 * rayon * asin(sqrt(pow(sin(((other.getLatitude() * pi / 180.0) - (this->getLatitude()  * pi / 180.0)) / 2.0), 2.0)  + cos((this->getLatitude() * pi / 180.0)) * cos((other.getLatitude() * pi / 180.0)) * pow(sin(((other.getLongitude() * pi / 180.0) - (this->getLongitude() * pi / 180.0)) / 2.0), 2.0))));
+	return (2.0 * rayon * asin(sqrt(
+		pow(sin(((other.getLatitude() * pi / 180.0) - (this->getLatitude()  * pi / 180.0)) / 2.0), 2.0) 
+		+ cos((this->getLatitude() * pi / 180.0)) 
+		* cos((other.getLatitude() * pi / 180.0)) 
+		* pow(sin(((other.getLongitude() * pi / 180.0) - (this->getLongitude() * pi / 180.0)) / 2.0), 2.0))));
 }
 
 std::ostream & operator<<(std::ostream & flux, const Coordonnees & p_coord)
