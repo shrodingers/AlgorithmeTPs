ab442589a2bcf6c1a402bbcc8445208a760b8b16
stefan_l <leon.stefani@epitech.eu>
2016-10-31 17:20:38 -0400

    [Refacto] Ajout commentaires réseau

--- AlgoTP2/reseau.cpp
+++ AlgoTP2/reseau.cpp
@@ -128,7 +128,9 @@ int Reseau::dijkstra(unsigned int numOrigine, unsigned int numDest, std::vector<
     std::unordered_map<unsigned int, unsigned int> predecesseurs;
     liste_sommets tmpGraph = m_sommets;
     unsigned int origin = INFINI;
-    auto findWay = [&predecesseurs, &chemin, numDest, numOrigine] () {
+
+    //Fonction qui construit le chemin avec les prédecesseurs
+    auto findWay = [this, &predecesseurs, &chemin, numDest, numOrigine] () {
         unsigned int current = numDest;
         while (current != numOrigine) {
             chemin.push_back(current);
@@ -136,19 +138,25 @@ int Reseau::dijkstra(unsigned int numOrigine, unsigned int numDest, std::vector<
         }
         chemin.push_back(current);
         std::reverse(chemin.begin(), chemin.end());
+        //Retrouve le coût total du plus court chemin. Utilise les fonction d'ordre supérieur de la STL pour cela.
+        return std::accumulate(std::next(chemin.begin()), chemin.end(), std::make_pair(0, chemin.begin()), [this] (std::pair<unsigned int,
+                std::vector<unsigned int>::iterator> const& prev, unsigned int const sommet) {
+            return std::make_pair(prev.first + m_sommets[*(prev.second)][sommet].first, std::next(prev.second));
+        }).first;
     };
 
+    //initialisation du graphe
     for (auto& elem: tmpGraph) {
         distances[elem.first] = INFINI;
     }
+
     distances[numOrigine] = 0;
     while (!tmpGraph.empty()) {
         origin = findMiniumum(tmpGraph, distances);
+
+        //Cas particulier qui indique la découverte du plus court chemin
         if (origin == numDest) {
-            findWay();
-            return std::accumulate(std::next(chemin.begin()), chemin.end(), std::make_pair(0, chemin.begin()), [this] (std::pair<unsigned int, std::vector<unsigned int>::iterator> const& prev, unsigned int const sommet) {
-                return std::make_pair(prev.first + m_sommets[*(prev.second)][sommet].first, std::next(prev.second));
-            }).first;
+            return findWay();
         } else if (origin == INFINI) {
             throw std::logic_error("Le chemin entre les deux sommets n'existe pas");
         }
@@ -163,11 +171,7 @@ int Reseau::dijkstra(unsigned int numOrigine, unsigned int numDest, std::vector<
         }
         tmpGraph.erase(origin);
     }
-    findWay();
-    //Retrouve le coût total du plus court chemin. Utilise les fonction d'ordre supérieur de la STL pour cela.
-    return std::accumulate(std::next(chemin.begin()), chemin.end(), std::make_pair(0, chemin.begin()), [this] (std::pair<unsigned int, std::vector<unsigned int>::iterator> const& prev, unsigned int const sommet) {
-        return std::make_pair(prev.first + m_sommets[*(prev.second)][sommet].first, std::next(prev.second));
-    }).first;
+    return findWay();
 }
 
 int Reseau::bellmanFord(unsigned int numOrigine, unsigned int numDest, std::vector<unsigned int> &chemin) throw (std::logic_error) {
@@ -175,6 +179,7 @@ int Reseau::bellmanFord(unsigned int numOrigine, unsigned int numDest, std::vect
     std::unordered_map<unsigned int, unsigned int> predecesseurs;
     bool hasChanged = true;
 
+    //Fonction qui construit le chemin avec les prédecesseurs
     auto findWay = [this, &predecesseurs, &distances, &chemin, numDest, numOrigine] () {
         unsigned int current = numDest;
 
@@ -192,17 +197,18 @@ int Reseau::bellmanFord(unsigned int numOrigine, unsigned int numDest, std::vect
         }
         chemin.push_back(current);
         std::reverse(chemin.begin(), chemin.end());
+        //Retrouve le coût total du plus court chemin. Utilise les fonction d'ordre supérieur de la STL pour cela.
+        return std::accumulate(std::next(chemin.begin()), chemin.end(), std::make_pair(0, chemin.begin()), [this] (std::pair<unsigned int, std::vector<unsigned int>::iterator> const& prev, unsigned int const sommet) {
+            return std::make_pair(prev.first + m_sommets[*(prev.second)][sommet].first, std::next(prev.second));
+        }).first;
     };
 
     for (auto& elem: m_sommets) distances[elem.first] = INFINI;
     distances[numOrigine] = 0;
     for (unsigned int i = 1; i != m_sommets.size() - 1; ++i) {
+        // Cas particulier indiquant que l'algorithme a déjà résolu tous les arcs
         if (!hasChanged) {
-            findWay();
-            //Retrouve le coût total du plus court chemin. Utilise les fonction d'ordre supérieur de la STL pour cela.
-            return std::accumulate(std::next(chemin.begin()), chemin.end(), std::make_pair(0, chemin.begin()), [this] (std::pair<unsigned int, std::vector<unsigned int>::iterator> const& prev, unsigned int const sommet) {
-                return std::make_pair(prev.first + m_sommets[*(prev.second)][sommet].first, std::next(prev.second));
-            }).first;
+            return findWay();
         }
         hasChanged = false;
         for (auto& sommet: m_sommets) {
@@ -216,11 +222,7 @@ int Reseau::bellmanFord(unsigned int numOrigine, unsigned int numDest, std::vect
             }
         }
     }
-    findWay();
-    //Retrouve le coût total du plus court chemin. Utilise les fonction d'ordre supérieur de la STL pour cela.
-    return std::accumulate(std::next(chemin.begin()), chemin.end(), std::make_pair(0, chemin.begin()), [this] (std::pair<unsigned int, std::vector<unsigned int>::iterator> const& prev, unsigned int const sommet) {
-        return std::make_pair(prev.first + m_sommets[*(prev.second)][sommet].first, std::next(prev.second));
-    }).first;
+    return findWay();
 }
 
 int Reseau::getComposantesFortementConnexes(std::vector<std::vector<unsigned int> > &composantes) const {
@@ -230,6 +232,7 @@ int Reseau::getComposantesFortementConnexes(std::vector<std::vector<unsigned int
     std::list<unsigned int> visitStack;
     liste_sommets reversed = getReverseGraph();
 
+    //Fonction récursive de visite en profondeur
     std::function<void(unsigned int)> visit = [this, &visit, &visited, &visitStack] (unsigned int sommet) {
         if (visited.find(sommet) == visited.end()) {
             visited.insert(sommet);
@@ -240,6 +243,7 @@ int Reseau::getComposantesFortementConnexes(std::vector<std::vector<unsigned int
         }
     };
 
+    // Fonction récursive de visite du graphe inverse en profondeur avec assignantion des composantes fortement connexes
     std::function<void(unsigned int, unsigned int)> assign = [&assigned, &assign, &reversed, &assignedList] (unsigned int sommet, unsigned int root) {
         if (assignedList.find(sommet) == assignedList.end()) {
             assigned[root].push_back(sommet);
@@ -250,17 +254,24 @@ int Reseau::getComposantesFortementConnexes(std::vector<std::vector<unsigned int
         }
     };
 
+    //visite du graphe en profondeur
     for (auto& elem: m_sommets) {
         visit(elem.first);
     }
+
+    // visite du graphe inverse en profondeur
     for (auto& elem: visitStack) {
         assign(elem, elem);
     }
 
+    // Récupération des composantes fortement connexes
     for (auto& elem: assigned) {
         composantes.push_back(elem.second);
     }
 
+
+    //Commentaire : Retourne la somme du coût des composantes fortement connexes, gardé sous la main au cas où
+    /*
     // calcule le coût total de toutes les composante fortement connexes
     return std::accumulate(composantes.begin(), composantes.end(), 0, [this] (unsigned int const cout, std::vector<unsigned int> const& elem) {
         // calcule le coût total d'une composante fortement connexe
@@ -274,8 +285,11 @@ int Reseau::getComposantesFortementConnexes(std::vector<std::vector<unsigned int
             });
         });
         return total;
-    });
-    return 0;
+    });*/
+
+    //Retourne la taille du vecteur contenat les composantes fortement connexes, la taille d'un vecteur étant dépendante de l'implémentation,
+    // le cast est ici nécessaire, sachant que la limite de composantes fortements connexes pouvant être trouvée ici sera numeric_limits<int>::max
+    return static_cast<unsigned int>(composantes.size());
 }
 
 bool Reseau::estFortementConnexe() const {
@@ -299,6 +313,9 @@ liste_sommets Reseau::getReverseGraph() const {
     return reversed;
 }
 
+//Les fonctions suivantes en print* son utilisées pour sérialiser un graphe au format dot dans un fichier et ainsi pouvoir en avoir un rendu graphique
+
+//Sérialise un graphe
 void Reseau::printGraph(std::ostream &os, liste_sommets const &graph) {
     os << "digraph myGraph {" << std::endl;
     for (auto& sommet: graph) {
@@ -309,6 +326,7 @@ void Reseau::printGraph(std::ostream &os, liste_sommets const &graph) {
     os << "}";
 }
 
+// sérialise le graphe d'un réseau
 void Reseau::print(std::ostream &os) {
     os << "digraph myGraph {" << std::endl;
     for (auto& sommet: m_sommets) {
@@ -319,6 +337,8 @@ void Reseau::print(std::ostream &os) {
     os << "}";
 }
 
+
+//sérialise le graphe d'un réseau et colorise le chemin des noeuds en rouge
 void Reseau::print(std::ostream &os, std::vector<unsigned int> const &chemin) {
     os << "digraph myGraph {" << std::endl;
     for (auto& sommet: m_sommets) {
@@ -338,6 +358,7 @@ void Reseau::print(std::ostream &os, std::vector<unsigned int> const &chemin) {
     os << "}";
 }
 
+//sérialise le graphe d'un réseau et colorise les arcs des composantes fortement connexes en bleu
 void Reseau::printConnexes(std::ostream &os, std::vector<std::vector<unsigned int> > const &compo) {
     os << "digraph myGraph {" << std::endl;
     for (auto& sommet: m_sommets) {
